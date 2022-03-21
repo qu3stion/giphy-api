655df211a8bd7ec967488f5196973e788eaff8e2
Braun Payne <84481864+qu3stion@users.noreply.github.com>
2022-03-20 20:45:16 -0400

    Delete src directory

--- src/services/constants.js
+++
@@ -1,2 +0,0 @@
-export const RANDOM_URL = "api.giphy.com/v1/gifs/random?api_key=eMqPe5p2pUDsWjEMuk2XOfyL1O7tjYpC"
-export const TRENDING_URL = "api.giphy.com/v1/gifs/trending?api_key=eMqPe5p2pUDsWjEMuk2XOfyL1O7tjYpC"
